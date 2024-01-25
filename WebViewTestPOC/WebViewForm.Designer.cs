623849ff56beedfd9fd64495763804429244c451
ShibuyaKosuke <github@shibuyakosuke.com>
2024-01-25 11:18:45 +0900

    プロジェクト ファイルを追加します。

---
+++ WebViewTestPOC/WebViewForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WebViewTestPOC
+{
+    partial class WebViewForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(45, 12);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(662, 19);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "https://qiita.com/signup";
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Location = new System.Drawing.Point(12, 15);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(27, 12);
+            this.lblUrl.TabIndex = 1;
+            this.lblUrl.Text = "URL";
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(713, 10);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(75, 23);
+            this.btnMove.TabIndex = 2;
+            this.btnMove.Text = "移動";
+            this.btnMove.UseVisualStyleBackColor = true;
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(12, 37);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(776, 401);
+            this.webView.TabIndex = 3;
+            this.webView.ZoomFactor = 1D;
+            this.webView.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webView_NavigationCompleted);
+            // 
+            // WebViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "WebViewForm";
+            this.Text = "WebViewForm";
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.Button btnMove;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+    }
+}
+
---
+++ WebViewTestPOC/WebViewForm.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebViewTestPOC
+{
+    public partial class WebViewForm : Form
+    {
+        private bool navigationCompleted = false;
+
+        public WebViewForm()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+
+        private async void InitializeWebView()
+        {
+            webView.CoreWebView2InitializationCompleted += webView_CoreWebView2InitializationCompleted;
+            webView.NavigationCompleted += webView_NavigationCompleted;
+            await webView.EnsureCoreWebView2Async();
+
+            webView.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
+        }
+
+        private void webView_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            // WebView2にHTMLをセットする
+            string htmlContent = "<html><body><form><input type='text' name='txt' /><input type='text' name='url' /></form></body></html>";
+            webView.CoreWebView2.NavigateToString(htmlContent);
+        }
+
+        private async void webView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (!navigationCompleted)
+            {
+                // フラグを設定
+                navigationCompleted = true;
+
+                // WebView2のナビゲーションが完了したら、非同期メソッドを呼び出す
+                await EditAndSetHTMLAsync();
+            }
+        }
+
+        private async Task EditAndSetHTMLAsync()
+        {
+            // WebView2でJavaScriptを実行してHTMLを編集する
+            await webView.CoreWebView2.ExecuteScriptAsync("document.querySelector('form').addEventListener('click', function(event) { window.chrome.webview.postMessage(event.target.getAttribute('name')); });");
+        }
+
+        private void CoreWebView2_WebMessageReceived(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            // JavaScriptからのメッセージを受信
+            string message = e.TryGetWebMessageAsString();
+
+            // メッセージを解析してクリックされた要素などの情報を取得
+            MessageBox.Show($"Clicked Element: {message}");
+        }
+    }
+}
