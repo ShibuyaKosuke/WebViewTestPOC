c86dbcd1c4165b34c498f369ffa0eaebd310002e
TSKGunGun <stonebeach.dakar@gmail.com>
2024-02-09 10:12:58 +0900

    機能追加

--- WebViewTestPOC/WebViewForm.Designer.cs
+++ WebViewTestPOC/WebViewForm.Designer.cs
@@ -31,8 +31,14 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnOpenCsv = new System.Windows.Forms.Button();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.WebView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_url = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.WebView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -49,7 +55,7 @@
             // btnOpenCsv
             // 
             this.btnOpenCsv.Enabled = false;
-            this.btnOpenCsv.Location = new System.Drawing.Point(45, 12);
+            this.btnOpenCsv.Location = new System.Drawing.Point(12, 84);
             this.btnOpenCsv.Name = "btnOpenCsv";
             this.btnOpenCsv.Size = new System.Drawing.Size(113, 23);
             this.btnOpenCsv.TabIndex = 4;
@@ -65,15 +71,45 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_url);
             this.splitContainer1.Panel1.Controls.Add(this.textBox1);
             this.splitContainer1.Panel1.Controls.Add(this.btnOpenCsv);
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.WebView);
-            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
-            this.splitContainer1.SplitterDistance = 223;
+            this.splitContainer1.Size = new System.Drawing.Size(948, 533);
+            this.splitContainer1.SplitterDistance = 264;
             this.splitContainer1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "URL";
+            // 
+            // txt_url
+            // 
+            this.txt_url.Location = new System.Drawing.Point(12, 30);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(239, 19);
+            this.txt_url.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 150);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 371);
+            this.textBox1.TabIndex = 5;
             // 
             // WebView
             // 
@@ -84,23 +120,54 @@
             this.WebView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.WebView.Location = new System.Drawing.Point(0, 0);
             this.WebView.Name = "WebView";
-            this.WebView.Size = new System.Drawing.Size(573, 450);
+            this.WebView.Size = new System.Drawing.Size(680, 533);
             this.WebView.TabIndex = 4;
             this.WebView.ZoomFactor = 1D;
             // 
-            // textBox1
+            // button1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 41);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(198, 397);
-            this.textBox1.TabIndex = 5;
+            this.button1.Location = new System.Drawing.Point(14, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "サンプルページを開く";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(132, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "入力したURLに遷移";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "操作ログ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(132, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "操作ログリセット";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // WebViewForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(948, 533);
             this.Controls.Add(this.splitContainer1);
             this.Name = "WebViewForm";
             this.Text = "WebViewForm";
@@ -120,6 +187,12 @@
         private System.Windows.Forms.SplitContainer splitContainer1;
         private Microsoft.Web.WebView2.WinForms.WebView2 WebView;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WebViewTestPOC/WebViewForm.cs
+++ WebViewTestPOC/WebViewForm.cs
@@ -15,6 +15,7 @@ namespace WebViewTestPOC
 
         public WebViewForm()
         {
+            this.WindowState = FormWindowState.Maximized;
             InitializeComponent();
             InitializeWebView();
         }
@@ -33,23 +34,38 @@ namespace WebViewTestPOC
 
         private void WebView_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
         {
+            NavigationToSample();
+        }
+
+        private void NavigationToSample()
+        {
+            navigationCompleted = false;
             // WebView2にHTMLをセットする
             string htmlContent = "<html><head></head><body><form><input type='text' name='txt' /><input type='text' name='url' /></form></body></html>";
             WebView.CoreWebView2.NavigateToString(htmlContent);
+        }
+
+        private async void NavigationToUrl(string url)
+        {
+            navigationCompleted = false;
+
+            // WebView2にURLをセットする
+            WebView.CoreWebView2.Navigate(url);
+
         }
 
         private async void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
         {
             if (!navigationCompleted)
             {
-                // フラグを設定
-                navigationCompleted = true;
-
                 // WebView2のナビゲーションが完了したら、非同期メソッドを呼び出す
                 await EditAndSetHTMLAsync();
 
                 // Log
                 await AddLogStoker();
+
+                // フラグを設定
+                navigationCompleted = true;
             }
 
             btnOpenCsv.Enabled = true;
@@ -156,6 +172,26 @@ namespace WebViewTestPOC
             {
                 throw new Exception("指定したファイルは存在しません。");
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt_url.Text = "";
+            NavigationToSample();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string url = txt_url.Text;
+            if (url != "")
+            {
+                NavigationToUrl(url);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
         }
     }
 }
